85694b545d4f8682f978eb903626546ab6869e3a
anck <myscoob@gmail.com>
2015-01-03 20:18:55 -0500

    1. Added text boxes in Index.php. 2. Moved from Classes to ID for CSS

--- Index.php
+++ Index.php
@@ -7,10 +7,63 @@
 <title> Add 'em up Again!</title>
 </head>
 <body>
+<script>
+function addRow2(tableID)
+{
+	var table=document.getElementById(tableID);
+	var rowCount=table.rows.length;
+	var row=table.insertRow(rowCount);
+	var colCount=table.rows[0].cells.length;
+	//document.write(colCount);
+	for(var i=0;i<colCount;i++)
+	{
+		var newcell=row.insertCell(i);
+		newcell.innerHTML=table.rows[2].cells[i].innerHTML;
+		switch(newcell.childNodes[0].type)
+		{
+			case"text":
+				newcell.childNodes[0].value="";
+			break;
+			case"checkbox":newcell.childNodes[0].checked=false;
+			break;
+			case"select-one":newcell.childNodes[0].selectedIndex=0;
+			break;
+		}
+	}
+}
 
+function deleteRow(tableID)
+	{
+		try
+		{
+			var table=document.getElementById(tableID);
+			var rowCount=table.rows.length;
+			for(var i=0;i<rowCount;i++)
+			{
+				var row=table.rows[i];
+				var chkbox=row.cells[0].childNodes[0];
+				if(null!=chkbox&&true==chkbox.checked)
+				{
+					table.deleteRow(i);
+					rowCount--;
+					i--;
+				}
+			}
+		}
+		catch(e)
+		{alert(e);}
+	}
+</script>
+
+<!-- Self submit to php -->
 <form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-<table class="tg">
+<!-- Calling Javascipt methods -->
+<input type="button" value="Add Row" onclick="addRow2('dataTable')">
+<input type="button" value="Delete Row" onclick="deleteRow('dataTable')">
+
+<table class="tg" id="dataTable">
   <tr>
+  	<th></th>
     <th >Prioirty</th>
     <th>Package Name</th>
     <th>Receieved Date</th>
@@ -21,29 +74,31 @@
     <th>Notes</th>
   </tr>
   <tr>
-    <td class = "tg-pr">3</td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
+  	<td><input type="checkbox" name="chk"></td>
+    <td id="Priority">3</td>
+    <td><input type="text" id="hfname" value="ICAWS750WX86X64020"></td>
+    <td><input type="date" id="hfdate"></td>
+    <td><input type="text" id="hflead" value="Anchitk"></td>
+    <td><input type="text" id="hfptr" value="9"></td>
+    <td><input type="text" id="hfhtr" value="0"></td>
+    <td><input type="text" id="hftype" value="Private VDA"></td>
+    <td><input type="text" id="hftype" value="NA"></td>
   </tr>
   <tr>
-    <td class = "tg-pr">3</td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
+  	<td><input type="checkbox" name="chk"></td>
+    <td id="Priority">3</td>
+    <td><input type="text" id="hfname" value="ICAWS750WX86X64020"></td>
+    <td><input type="date" id="hfdate"></td>
+    <td><input type="text" id="hflead" value="Anchitk"></td>
+    <td><input type="text" id="hfptr" value="9"></td>
+    <td><input type="text" id="hfhtr" value="0"></td>
+    <td><input type="text" id="hftype" value="Private VDA"></td>
+    <td><input type="text" id="hftype" value="NA"></td>
   </tr>
 </table>
 </form>
-<?php
 
+<?php
 if(isset($_POST['submit']))
 {	
 	
